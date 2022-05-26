aa9548f5cec2d604056370ca4aef661c2611012b
Andrew Stakhov <andrew.stakhov@gmail.com>
2022-05-25 20:53:18 -0400

    Remove startup file

---
+++ src/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Steeltoe.Connector.CloudFoundry;
+
+namespace Articulate;
+
+public static class ExtensionMethods
+{
+    public static void EnsureMigrationOfContext<T>(this IApplicationBuilder app) where T : DbContext
+    {
+        var context = app.ApplicationServices.CreateScope().ServiceProvider.GetService<T>();
+        context.Database.Migrate();
+    }
+
+    public static bool IsServiceBound<T>(this IConfiguration configuration) where T : class => CloudFoundryServiceInfoCreator.Instance(configuration).GetServiceInfos<T>().Any();
+
+}
